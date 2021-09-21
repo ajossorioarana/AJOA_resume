6531f1dcc7994625a7529b4ad5a6bff0e25c5a44
Arturo J. Ossorio Arana <arturoa91@gmail.com>
2021-09-21 18:17:51 -0300

    Apply major format modifications

--- resume.py
+++ resume.py
@@ -1,5 +1,53 @@
 from fpdf import FPDF
 
+
+def write_header(header_title: str, width: int, height: int):
+        pdf.set_font("Ubuntu Bold", "", 12)
+        pdf.set_text_color(0, 0, 0)
+        #pdf.set_draw_color(232,212,148)
+        pdf.set_draw_color(62, 171, 134)
+        pdf.set_line_width(0.3)
+        pdf.cell(w=width, h = height, txt=header_title, border="B", fill=False)
+        pdf.ln(8)
+
+def write_exp(company: str, role: str, link: str, period: str, place: str, keypoints: list, width: int, height: int):
+        pdf.set_font("Ubuntu Medium", "", 10)
+        pdf.set_text_color(0, 15, 75)
+        pdf.cell(w=width, h=height, txt="{0} - {1}".format(company, role), link=link)
+        pdf.ln(5)
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Ubuntu Medium", "", 8)
+        pdf.cell(w=width, h=height, txt="{0} | {1}".format(period, place))
+        pdf.ln(5)
+        pdf.set_font("Ubuntu Light", "", 8)
+        for keypoint in keypoints:
+                pdf.write(h=height, txt="+ {0}".format(keypoint))
+                pdf.ln(5)
+        pdf.ln(3)
+
+def write_education(title: str, university: str, grad_date: str, width: int, height: int):
+        pdf.set_font("Ubuntu Medium", "", 10)
+        pdf.set_text_color(0, 15, 75)
+        pdf.cell(w=width, h=height, txt=title)
+        pdf.ln(5)
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Ubuntu Light", "", 8)
+        pdf.cell(w=width, h=height, txt=(university + " - " + grad_date))
+        pdf.ln(8)
+
+def write_project(title: str, keypoints: list, width: int, height: int):
+        pdf.set_font("Ubuntu Medium", "", 10)
+        pdf.set_text_color(0, 15, 75)
+        pdf.cell(w=width, h=height, txt=title)
+        pdf.ln(5)
+        pdf.set_text_color(0, 0, 0)
+        pdf.set_font("Ubuntu Light", "", 8)
+        for keypoint in keypoints:
+                        pdf.write(h=height, txt="+ {0}".format(keypoint))
+                        pdf.ln(5)
+        pdf.ln(3)
+
+
 # TEXT VARIABLES
 
 # Contact and general info
@@ -9,14 +57,13 @@ role = "Ecohydrologist - Data Scientist"
 city = "San Martín de los Andes"
 prov_country = "Neuquén, Argentina"
 mail = "arturoa91@gmail.com"
-cellphone = "+54 9 11 3636823"
-linkedin = "LinkedIn: /in/ajossorioarana"
-github = "GitHub: /ajossorioarana"
+cellphone = "+54 9 113 636 8323"
+linkedin = "linkedin.com/in/ajossorioarana"
+github = "github.com/ajossorioarana"
 website = "ajossorioarana.github.io"
 
-desc_ln1 = "Ecohydrologist geared towards data science, with an analytical background and experience working with"
-desc_ln2 = "stakeholders and managing teams. Currently increasing mastery in Machine Learning. I will be uploading"
-desc_ln3 = "related content through my GitHub account and website."
+desc = "Ecohydrologist geared towards data science, with an analytical background and experience working with stakeholders and managing teams. Currently increasing mastery in Machine Learning. I will be uploading related content through my GitHub account and website."
+
 
 # Skills
 skills = [
@@ -45,8 +92,7 @@ interests = [
         "Hydraulics"
         ]
 
-# Education
-edu_heading = "EDUCATION"
+# Education info
 
 edu_1_title = "M.SC. in Ecohydrology"
 edu_1_place = "UNLP"
@@ -56,50 +102,66 @@ edu_2_title = "Engineer's degree, Civil Engineer"
 edu_2_place = "UBA"
 edu_2_grad_date = "Graduated 2018"
 
-# Experience
-exp_heading = "EXPERIENCE"
+# Experience info
 
 exp_1_role = "Chief of Technical Department"
 exp_1_company = "EcoAqua"
 exp_1_company_link = "https://ecoaqua.com.ar/"
 exp_1_period = "Feb. 2020 - Present"
-exp_1_kp1 = "Diagnosed and treated customer's lakes for algae, weeds and water quality issues."
-exp_1_kp2 = "Implemented new budgeting process, based on limnological factors, incresing revenue by > 30%."
-exp_1_kp3 = "Worked together with COO, improving KPI performance, mainly client retention rate."
+exp_1_place = "Buenos Aires, Argentina"
+exp_1_keypoints = [
+                "Diagnosed and treated customer's lakes for algae, weeds and water quality issues.",
+                "Implemented new budgeting process, based on limnological factors, incresing revenue by > 30%.",
+                "Worked together with COO, improving KPI performance, mainly client retention rate."
+                ]
 
 exp_2_role = "Aquatic Specialitst"
 exp_2_company = "EcoAqua"
 exp_2_company_link = "https://ecoaqua.com.ar/"
 exp_2_period = "Jun. 2017 - Jan. 2020"
-exp_2_kp1 = "Managed five 2-people teams to ensure all services are completed on schedule."
-exp_2_kp2 = "Serviced customer’s lakes to provide superior quality and service in the fulfillment of all customer needs."
-exp_2_kp3 = "Deployed maintainance routine for vehicles and machinery, decreasing workflow interruptions by 40%."
+exp_2_place = "Buenos Aires, Argentina"
+exp_2_keypoints = [
+                "Managed five 2-people teams to ensure all services are completed on schedule.",
+                "Serviced customer’s lakes to provide superior quality and service in the fulfillment of all customer needs.",
+                "Deployed maintainance routine for vehicles and machinery, decreasing workflow interruptions by 40%."
+                ]
 
 exp_3_role = 'Graduate Teaching Assistant (Course "Applied Hydraulics")'
 exp_3_company = "UBA"
+exp_3_company_link = "https://fi.uba.ar/"
 exp_3_period = "Aug. 2017 - Present"
-exp_3_kp1 = "Been asigned to > 100 students, answering queries and grading assignments." 
-exp_3_kp2 = "Aided professors in examinations and dictated several practical lectures."
-exp_3_kp3 = "Syllabus: Pressurized pipe flow, pump selection, water hammer, open channel flow and weir design."
+exp_3_place = "Buenos Aires, Argentina"
+exp_3_keypoints = [
+                "Been asigned to > 100 students, answering queries and grading assignments.",
+                "Aided professors in examinations and dictated several practical lectures.",
+                "Syllabus: Pressurized pipe flow, pump selection, water hammer, open channel flow and weir design."
+                ]
 
 exp_4_role = "Bid/Tender Assistant"
 exp_4_company = "Buenos Aires City Government"
+exp_4_company_link = "https://www.buenosaires.gob.ar/desarrollourbano/desarrollo/planes/plan-hidraulico"
 exp_4_period = "Jun. 2016 - May. 2017"
-exp_4_kp1 = "Cash-flow and finance audit of IBRD-loan WASH project (Arroyo Vega Drainage Tunnel > $145M)."
-exp_4_kp2 = "Elaboration of bidding documentation in accordance to IBRD norms and recommendations."
+exp_4_place = "Buenos Aires, Argentina"
+exp_4_keypoints = [
+                "Cash-flow and finance audit of IBRD-loan WASH project (Arroyo Vega Drainage Tunnel > $145M).",
+                "Elaboration of bidding documentation in accordance to IBRD norms and recommendations."
+                ]
 
-# Projects
-proj_heading = "PROJECTS/PUBLICATIONS"
+# Projects info
 
 proj_1_title = "EDA on Argentine lakes and reservoirs dataset"
-proj_1_tech = "Python / Seaborn / EDA / CSV / DataViz"
-proj_1_comment_1 = "Polished, visualized and interpreted resultes of Argentine lakes dataset."
-proj_1_comment_2 = "Obtained Carlson's trophic state index (TSI) for waterboides and presented relevant analysis."
+proj_1_keypoints = [
+                "Python / Seaborn / EDA / CSV / DataViz",
+                "Polished, visualized and interpreted resultes of Argentine lakes dataset.",
+                "Obtained Carlson's trophic state index (TSI) for waterboides and presented relevant analysis."
+                ]
 
 proj_2_title = "Watershed delineation of Chimehuín river, Patagonia, Argentina"
-proj_2_tech = "QGIS / GRASS"
-proj_2_comment_1 = "Developed map of Chimehuín river basin using processed DEM, with lakes and watercourses marked on it."
-proj_2_comment_2 = "Created as part of dissertation about exotic willow (Salix fragilis) invasion of patagonian rivers."
+proj_2_keypoints = [
+                "QGIS / GRASS",
+                "Developed map of Chimehuín river basin using processed DEM, with lakes and watercourses marked on it.",
+                "Created as part of dissertation about exotic willow (Salix fragilis) invasion of patagonian rivers."
+                ]
 
 
 # ARMADO DE PDF
@@ -110,193 +172,75 @@ pdf.add_font("Ubuntu Medium", "", r"C:\Users\aoa91\AppData\Local\Microsoft\Windo
 pdf.add_font("Ubuntu Bold", "", r"C:\Users\aoa91\AppData\Local\Microsoft\Windows\Fonts\Ubuntu-Bold.ttf", uni=True)
 pdf.add_page()
 
-width_first = 150
-h_cells = 10
-
+width = 189
+h_cells = 6
 
 # NAME AND ROLE
-pdf.ln(5)
+
+pdf.ln(3)
 pdf.set_text_color(0, 0, 0)
 pdf.set_font("Ubuntu Medium", "", 18)
-pdf.cell(w=width_first, h = h_cells, txt=name)
-pdf.ln(10)
+pdf.cell(w=width, h=h_cells, txt=name)
+pdf.ln(8)
+pdf.set_font("Ubuntu Light", "", 14)
+pdf.cell(w=width, h=h_cells, txt=role)
 
-pdf.set_font("Ubuntu Light", "", 16)
-pdf.cell(w=width_first, h = h_cells, txt=role)
-pdf.set_line_width(0.5)
+pdf.ln(8)
 
-pdf.ln(12)
+# CONTACT INFO 
+pdf.set_font("Ubuntu Medium", "", 8)
+pdf.set_text_color(0, 15, 75)
+pdf.write(h_cells, mail, link=mail)
+pdf.write(h_cells, txt=(" "*3 + "|" + " "*3))
+pdf.write(h_cells, cellphone)
+pdf.write(h_cells, txt=(" "*3 + "|" + " "*3))
+pdf.write(h_cells, linkedin, link=linkedin)
+pdf.write(h_cells, txt=(" "*3 + "|" + " "*3))
+pdf.write(h_cells, github, link=github)
+pdf.write(h_cells, txt=(" "*3 + "|" + " "*3))
+pdf.write(h_cells, website, link=website)
+pdf.ln(8)
 
-pdf.set_fill_color(215, 215, 255)
-pdf.line(10, 37, width_first + 10, 37)
+# DESCRIPTION
+
+write_header("ABOUT ME", width, h_cells)
+pdf.ln(1)
 pdf.set_font("Ubuntu Light", "", 9)
-pdf.cell(w=width_first, h = h_cells, txt=desc_ln1)
-pdf.ln(6)
-pdf.cell(w=width_first, h = h_cells, txt=desc_ln2)
-pdf.ln(6)
-pdf.cell(w=width_first, h = h_cells, txt=desc_ln3)
-pdf.ln(13)
-pdf.line(10, 59, width_first + 10, 59)
-
-pdf.set_line_width(43)
-pdf.set_draw_color(30, 80, 125)
-pdf.line((width_first + 35), 30, (width_first + 35), 270)
-
-# Contact information
-pdf.set_text_color(255, 255, 255)
-pdf.set_font("Ubuntu Medium", "", 8)
-pdf.text(width_first + 17.5, 20, txt=city)
-pdf.text(width_first + 17.5, 25, txt=prov_country)
-pdf.text(width_first + 17.5, 35, txt=mail)
-pdf.text(width_first + 17.5, 40, txt=cellphone)
-pdf.text(width_first + 17.5, 45, txt=linkedin)
-pdf.text(width_first + 17.5, 50, txt=github)
-pdf.text(width_first + 17.5, 60, txt=website)
-
-# Skills
-pdf.set_font("Ubuntu Medium", "", 10)
-pdf.text(width_first + 17.5, 80, txt="Skills:")
-pdf.set_font("Ubuntu Medium", "", 8)
-for i in range(len(skills)):
-        pdf.text(width_first + 20, (85 + i*5), txt=("+ " + skills[i]))
-
-pdf.set_font("Ubuntu Medium", "", 10)
-aux_depth = 85 + len(skills)*5 + 20
-pdf.text(width_first + 17.5, aux_depth, txt="Areas of interest:")
-pdf.set_font("Ubuntu Medium", "", 8)
-for i in range(len(interests)):
-        pdf.text(width_first + 20, (aux_depth + 5 + i*5), txt=("+ " + interests[i]))
-
+pdf.write(h=h_cells - 1, txt=desc)
+pdf.ln(8)
 
 
 # EXPERIENCE
 
-pdf.set_font("Ubuntu Bold", "", 12)
-pdf.set_text_color(30, 80, 125)
-pdf.cell(w=width_first, h = (h_cells - 2), txt=exp_heading, fill=True)
-pdf.ln(8)
+write_header("EXPERIENCE", width, h_cells)
+write_exp(exp_1_company, exp_1_role, exp_1_company_link, exp_1_period, exp_1_place, exp_1_keypoints, width, h_cells)
+write_exp(exp_2_company, exp_2_role, exp_2_company_link, exp_2_period, exp_2_place, exp_2_keypoints, width, h_cells)
+write_exp(exp_3_company, exp_3_role, exp_3_company_link, exp_3_period, exp_3_place, exp_3_keypoints, width, h_cells)
+write_exp(exp_4_company, exp_4_role, exp_4_company_link, exp_4_period, exp_4_place, exp_4_keypoints, width, h_cells)
 
-pdf.set_text_color(0, 0, 0)
-
-pdf.set_font("Ubuntu Medium", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt="{0} - {1}".format(exp_1_company, exp_1_role), link=exp_1_company_link)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt=exp_1_period)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 8)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_1_kp1))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_1_kp2))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_1_kp3))
-
-pdf.ln(8)
-
-pdf.set_font("Ubuntu Medium", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt="{0} - {1}".format(exp_2_company, exp_2_role), link=exp_2_company_link)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt=exp_2_period)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 8)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_2_kp1))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_2_kp2))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_2_kp3))
-
-pdf.ln(8)
-
-pdf.set_font("Ubuntu Medium", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt="{0} - {1}".format(exp_3_company, exp_3_role))
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt=exp_3_period)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 8)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_3_kp1))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_3_kp2))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_3_kp3))
-
-pdf.ln(8)
-
-pdf.set_font("Ubuntu Medium", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt="{0} - {1}".format(exp_4_company, exp_4_role))
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt=exp_4_period)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 8)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_4_kp1))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(exp_4_kp2))
-
-pdf.ln(10)
 
 # EDUCATION
 
-pdf.set_font("Ubuntu Bold", "", 12)
-pdf.set_text_color(30, 80, 125)
-pdf.cell(w=width_first, h=(h_cells - 2), txt=edu_heading, fill=True)
-pdf.ln(8)
-
-pdf.set_text_color(0, 0, 0)
-
-pdf.set_font("Ubuntu Medium", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt=edu_1_title)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 9)
-pdf.cell(w=width_first, h = h_cells, txt=(edu_1_place + " - " + edu_1_grad_date))
-
-pdf.ln(8)
-
-pdf.set_font("Ubuntu Medium", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt=edu_2_title)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 9)
-pdf.cell(w=width_first, h = h_cells, txt=(edu_2_place + " - " + edu_2_grad_date))
-
-pdf.ln(10)
+write_header("EDUCATION", width, h_cells)
+write_education(edu_1_title, edu_1_place, edu_1_grad_date, width, h_cells)
+write_education(edu_2_title, edu_2_place, edu_2_grad_date, width, h_cells)
 
 
 # PROJECTS 
 
-pdf.set_font("Ubuntu Bold", "", 12)
-pdf.set_text_color(30, 80, 125)
-pdf.cell(w=width_first, h = (h_cells - 2), txt=proj_heading, fill=True)
-pdf.ln(8)
+write_header("PROJECTS/PUBLICATIONS", width, h_cells)
+write_project(proj_1_title, proj_1_keypoints, width, h_cells)
+write_project(proj_2_title, proj_2_keypoints, width, h_cells)
 
-pdf.set_text_color(0, 0, 0)
 
-pdf.set_font("Ubuntu Medium", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt=proj_1_title)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 8)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(proj_1_tech))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(proj_1_comment_1))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(proj_1_comment_2))
+# Footer
 
-pdf.ln(8)
-
-pdf.set_font("Ubuntu Medium", "", 10)
-pdf.cell(w=width_first, h = h_cells, txt=proj_2_title)
-pdf.ln(5)
-pdf.set_font("Ubuntu Light", "", 8)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(proj_2_tech))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(proj_2_comment_1))
-pdf.ln(5)
-pdf.cell(w=width_first, h = h_cells, txt="+ {0}".format(proj_2_comment_2))
-
-pdf.ln(9)
-
+pdf.ln(3)
 pdf.set_text_color(100, 100, 100)
 pdf.set_font("Ubuntu Light", "I", 7)
-pdf.cell(w=width_first, h = h_cells-4, txt=header)
+pdf.cell(w=width, h=h_cells-4, txt=header)
 
-pdf.output("AJOA_Resume.pdf", "F")
+
+# Export as PDF
+
+pdf.output("Ossorio_Arturo_Resume.pdf", "F")
